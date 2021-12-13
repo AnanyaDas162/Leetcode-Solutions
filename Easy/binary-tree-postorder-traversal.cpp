eed753c609495d9e5e12ea333a3767c06b00866f
ANANYA DAS <84580932+AnanyaDas162@users.noreply.github.com>
2021-12-13 11:33:55 +0530

    Add files via upload
    Link of the Question  ::   https://leetcode.com/problems/binary-tree-postorder-traversal/

---
+++ Easy/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>solve(TreeNode* root, vector<int>v){
+        if (root == NULL)
+        {
+            return v;
+        }
+        v = solve (root -> left, v);
+        v = solve (root -> right, v);
+        v.push_back(root -> val);
+        return v;
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>v;
+        v = solve (root, v);
+        return v;
+    }
+};
